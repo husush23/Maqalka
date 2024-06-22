25d021668f7ac74fb4f94f1aeca11167ec34009c
husush23 <husseinkadare40@gmail.com>
2024-06-22 11:37:02 +0300

    Footer added

---
+++ components/Footer.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  return (
+    <footer className='bg-accent py-8 text-secondary'>
+      <div className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0'>
+          <div className='flex space-x-4'>
+            <Link href='/' className='text-gray-100 hover:text-red-400'>
+              Home
+            </Link>
+            <Link href='/about' className='text-gray-100 hover:text-red-400'>
+              About
+            </Link>
+            <Link href='/contact' className='text-gray-100 hover:text-red-400'>
+              Contact
+            </Link>
+            <Link href='/blogs' className='text-gray-100 hover:text-red-400'>
+              Blogs
+            </Link>
+          </div>
+          <div className='flex space-x-4'>
+            <Link
+              href='https://twitter.com'
+              className='text-gray-100 hover:text-red-400'
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </Link>
+            <Link
+              href='https://youtube.com'
+              className='text-gray-100 hover:text-red-400'
+            >
+              <FontAwesomeIcon icon={faYoutube} />
+            </Link>
+            <Link
+              href='https://facebook.com'
+              className='text-gray-100 hover:text-red-400'
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </Link>
+          </div>
+        </div>
+        <div className='mt-8 text-center text-gray-100'>
+          &copy; {new Date().getFullYear()} <strong>Maqalka.</strong> All rights
+          reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/Navabar.tsx
+++ components/Navabar.tsx
@@ -129,10 +129,10 @@ const Navbar = () => {
             </li>
             <li>
               <Link
-                href='/articles'
+                href='/blogs'
                 className='block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50'
               >
-                Articles
+                Blogs
               </Link>
             </li>
             <li>
