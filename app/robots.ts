a7271d4473c95d48cf379a312881f9a159290d78
husush23 <husseinkadare40@gmail.com>
2024-07-02 16:17:44 +0300

    Robt.txt configured

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import {MetadataRoute} from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [],
+      },
+    ],
+    sitemap: 'https://maqalka.vercel.app/sitemap.xml',
+  };
+}
